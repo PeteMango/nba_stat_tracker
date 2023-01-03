9e4581f167297a2239f6284ab7f3a3119ac257fb
Norman Chen <113641259+normanchenn@users.noreply.github.com>
2023-01-03 04:50:57 -0330

    changed class to className

--- frontend/src/component/Footer.js
+++ frontend/src/component/Footer.js
@@ -3,25 +3,25 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer class="flex w-screen fixed bottom-0 bg-opacity-100 pb-4 justify-center">
-      <ul class="flex flex-wrap items-center mt-3 text-sm text-black dark:text-gray-400 sm:mt-0">
+    <footer className="flex w-screen fixed bottom-0 bg-opacity-100 pb-4 justify-center">
+      <ul className="flex flex-wrap items-center mt-3 text-sm text-black dark:text-gray-400 sm:mt-0">
         <li>
-          <a href="/boxscores/today" class="mr-4 hover:underline md:mr-6 ">
+          <a href="/boxscores/today" className="mr-4 hover:underline md:mr-6 ">
             Today
           </a>
         </li>
         <li>
-          <a href="/search" class="mr-4 hover:underline md:mr-6">
+          <a href="/search" className="mr-4 hover:underline md:mr-6">
             Search
           </a>
         </li>
         <li>
-          <a href="/boxscores/date" class="mr-4 hover:underline md:mr-6">
+          <a href="/boxscores/date" className="mr-4 hover:underline md:mr-6">
             Boxscore
           </a>
         </li>
         <li>
-          <a href="#" class="hover:underline">
+          <a href="#" className="hover:underline">
             Login
           </a>
         </li>
