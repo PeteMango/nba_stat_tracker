1fec64e0df18d3e8dff468e27ee42c6a43311281
Norman Chen <113641259+normanchenn@users.noreply.github.com>
2023-01-03 05:09:56 -0330

    changed href to link

--- frontend/src/component/Footer.js
+++ frontend/src/component/Footer.js
@@ -6,24 +6,24 @@ function Footer() {
     <footer className="flex w-screen fixed bottom-0 bg-opacity-100 bg-white dark:bg-[#1e293b] pb-4 justify-center">
       <ul className="flex flex-wrap items-center justify-center mt-3 text-sm text-black dark:text-gray-400 sm:mt-0">
         <li>
-          <a href="/boxscores/today" className="mr-4 hover:underline md:mr-6">
+          <Link to="/boxscores/today" className="mr-4 hover:underline md:mr-6">
             Today
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/search" className="mr-4 hover:underline md:mr-6">
+          <Link to="/search" className="mr-4 hover:underline md:mr-6">
             Search
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/boxscores/date" className="mr-4 hover:underline md:mr-6">
+          <Link to="/boxscores/date" className="mr-4 hover:underline md:mr-6">
             Boxscore
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="#" className="hover:underline">
+          <Link to="#" className="hover:underline">
             Login
-          </a>
+          </Link>
         </li>
       </ul>
     </footer>
